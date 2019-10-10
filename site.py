b3c6d629ffc9ee970c4e38688b972062f29adb07
LucasI27 <luina.12042003@gmail.com>
2019-10-10 09:26:14 -0300

    templates

--- site.py
+++ site.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app=Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'pagina inicial'
+    return render_template ("index.html")
+
+@app.route('/posts')
+def posts():
+    return render_template ("posts.html")
+
+@app.route('/login')
+def login():
+    return render_template ("login.html")
 
 if __name__ =='__main__':
     app.run(debug=True)
